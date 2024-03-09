a626c54e2cc01f20d83f21904f71430f1d8d801b
a.deev <a.deev@cft.ru>
2024-03-09 16:46:42 +0700

    Added basis layout

---
+++ app/src/main/java/com/cinemaschedule/hall/HallScreen.kt
@@ -0,0 +1,36 @@
+package com.cinemaschedule.hall
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HallScreen() {
+	Title()
+	Description()
+	Screen()
+	SeatingArrangement()
+}
+
+@Composable
+private fun Title() {
+
+}
+
+@Composable
+private fun Description() {
+
+}
+
+@Composable
+private fun Screen() {
+
+}
+
+@Composable
+private fun SeatingArrangement() {
+
+}
+
+@Composable
+private fun Seat() {
+
+}
